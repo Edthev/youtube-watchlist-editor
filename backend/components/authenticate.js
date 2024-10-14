5128337667a7d4c8ff55e40354f60111103d9b73
Asden <edthevargas@gmail.com>
2024-10-14 11:29:37 -0400

    removed components/authenticate

--- backend/components/authenticate.js
+++
@@ -1,9 +0,0 @@
-const authenticate = async (req, res) => {
-   const SCOPES = [];
-   const CLIENT_ID = "";
-   const CLIENT_SECRET = "";
-   const REDIRECT_URL = "";
-   const STATE = "";
-   console.log(res);
-};
-module.exports = authenticate;
