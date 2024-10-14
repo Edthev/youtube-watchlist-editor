32eb6bbfb7a1170282aa1a0bb2ea316a5e01b9a6
Asden <edthevargas@gmail.com>
2024-10-14 10:58:22 -0400

    rename component folder to endpoints

---
+++ backend/endpoints/authenticate.js
@@ -0,0 +1,9 @@
+const authenticate = async (req, res) => {
+   const SCOPES = [];
+   const CLIENT_ID = "";
+   const CLIENT_SECRET = "";
+   const REDIRECT_URL = "";
+   const STATE = "";
+   console.log(res);
+};
+module.exports = authenticate;
