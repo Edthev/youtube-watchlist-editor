f94b5f8968a65344fae4cce4eba4003eb70a712b
Asden <edthevargas@gmail.com>
2024-10-23 10:15:14 -0400

    add list of playlists once logged in

---
+++ frontend/src/components/isLoggedIn.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+export default function isLoggedIn() {
+   const [playlistArr, setPlaylistArr] = useState([]);
+   useEffect(() => {
+      const handlePlaylists = async () => {
+         const getToken = (name) => {
+            const cookieArr = document.cookie.split(";");
+            for (let cookie of cookieArr) {
+               if (cookie.trim().startsWith(name + "=")) {
+                  return cookie.trim().slice(name.length + 1);
+               }
+            }
+            return false;
+         };
+         const API_TOKEN = getToken("access_token");
+         try {
+            await axios
+               .get("https://youtube.googleapis.com/youtube/v3/playlists", {
+                  params: {
+                     part: "snippet,contentDetails",
+                     maxResults: 5,
+                     mine: true,
+                  },
+                  headers: {
+                     Authorization: `Bearer ${API_TOKEN}`,
+                     Accept: "application/json",
+                  },
+               })
+               .then(async (response) => {
+                  const data = response.data.items;
+                  console.log("test", data);
+                  setPlaylistArr(data);
+                  return { data, test: "ya" };
+               });
+         } catch (err) {
+            console.error("err", err);
+            return { Error: "Can't fetch playlists", response: err };
+         }
+      };
+      handlePlaylists();
+   }, []);
+   return (
+      <div>
+         <p>logged in</p>
+         <div>
+            {playlistArr.map((playlist) => {
+               const name = playlist.snippet.title;
+               const imageURL = playlist.snippet.thumbnails.standard.url;
+               return (
+                  <div>
+                     <img src={imageURL} />
+                     <p>{name}</p>
+                  </div>
+               );
+            })}
+         </div>
+      </div>
+   );
+}
