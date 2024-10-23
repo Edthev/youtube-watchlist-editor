ee035d867336c04343f5423aa2b2778402f34985
Asden <edthevargas@gmail.com>
2024-10-23 10:21:08 -0400

    add googleOAuth redirect with scopes

--- backend/endpoints/redirectGoogleAuth.js
+++ backend/endpoints/redirectGoogleAuth.js
@@ -1,8 +1,17 @@
 const redirectGoogleAuth = async (req, res) => {
    try {
-      res.redirect("https://www.example.com");
+      const secrets = require("../assets/client_secret.json");
+      const CLIENT_ID = secrets.web.client_id;
+      const REDIRECT_URI = secrets.web.redirect_uris[0];
+      const SCOPES = ["https://www.googleapis.com/auth/youtube"];
+      console.log("redirecting...");
+      const googleOAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES.join(
+         "%20"
+      )}&access_type=offline`;
+      res.redirect(googleOAuthURL);
    } catch (err) {
-      res.send({ Error: "Redirect Error", Data: err });
+      console.log("err", err);
+      res.send({ Error: "Error Redirecting", Data: err });
    }
 };
 module.exports = redirectGoogleAuth;
